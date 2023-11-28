6ff2acae5499f9ffd723224a463af3991735a7bb
hall-e3 <hallenochasanda@gmail.com>
2023-11-28 14:41:07 +0300

    pages setup

---
+++ src/pages/index.jsx
@@ -0,0 +1,7 @@
+import Home from "./Home";
+import About from "./About";
+import Careers from "./Careers";
+import News from "./News";
+import Products from "./Products";
+import Services from "./Services";
+export { Home, About, Careers, News, Products, Services };
