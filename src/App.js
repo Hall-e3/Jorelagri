f95787a2f4054c5cdecc81a543695e6d56e5c4a6
hall-e3 <hallenochasanda@gmail.com>
2023-11-28 15:05:36 +0300

    pages setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import "./App.css";
+import { lazy } from "react";
+import { Supensing } from "./components";
+import { Route, Routes } from "react-router-dom";
+
+const News = Supensing(lazy(() => import("./pages/News")));
+const About = Supensing(lazy(() => import("./pages/About")));
+const Careers = Supensing(lazy(() => import("./pages/Careers")));
+const Products = Supensing(lazy(() => import("./pages/Products")));
+const Services = Supensing(lazy(() => import("./pages/Services")));
+const Home = Supensing(lazy(() => import("./pages/Home")));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">New site</header>
-    </div>
+    <Routes>
+      <Route path={["/", "/home"]} element={<Home />} />
+      <Route path="/news" element={<News />} />
+      <Route path="/careers" element={<Careers />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   );
 }
 
---
+++ src/components/index.jsx
@@ -0,0 +1,8 @@
+import Loader from "./loaders/Loader";
+import { Suspensing } from "./loaders/Suspensing";
+import Button from "./primitives/Button";
+import Spinner from "./loaders/Spinner";
+import Divider from "./primitives/Divider";
+import FetchLoading from "./loaders/FetchLoading";
+
+export { Loader, Spinner, Button, Divider, FetchLoading, Suspensing };
