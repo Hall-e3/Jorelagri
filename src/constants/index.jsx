c9020459b3cb29341ae65e25bb0a3258ecff4624
hall-e3 <hallenochasanda@gmail.com>
2023-11-28 19:58:42 +0300

    talk to us done

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,6 +1,7 @@
 const head_logo = require("../assets/images/olamagri-primary-logo.webp");
 const footer_logo = require("../assets/images/olamagri-footer-logo.webp");
+const talk = require("../assets/images/talk-to-us-icon.png");
 const careers_banner = require("../assets/images/careers-banner-b.png");
 const linkedin = require("../assets/images/linkedin.svg");
 const twitter = require("../assets/images/x-logo.svg");
-export { head_logo, careers_banner, footer_logo, twitter, linkedin };
+export { head_logo, careers_banner, footer_logo, twitter, linkedin, talk };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { footer_logo, head_logo, linkedin, twitter } from "../constants";
+import { footer_logo, head_logo, linkedin, talk, twitter } from "../constants";
+import { Button } from "../components";
 
 export default function Home() {
   const [first, setFirst] = useState(false);
@@ -15,7 +16,7 @@ export default function Home() {
   };
 
   return (
-    <div className="h-screen w-full border-t-orange-900">
+    <div className="h-screen w-full border-t-orange-800">
       <div className="w-full bg-secondary_orange h-20">
         <div className="max-w-[1580px] mx-auto h-full">
           <div className="h-full flex items-center justify-between gap-8">
@@ -40,7 +41,29 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="py-20 bg-[#2e2d2c]">
+      <div className="py-10 bg-[#2e2d2c] rounded-t-[100%]"></div>
+      <div className="pb-20 bg-[#2e2d2c]">
+        <div className="max-w-[1240px] mx-auto px-30">
+          <div className="w-full flex flex-col justify-center items-center gap-8">
+            <img
+              src={talk}
+              alt="talk to us"
+              className="h-30 w-30 object-contain"
+            />
+            <h4 className="text-white text-5xl font-semibold">Talk to Us</h4>
+            <p className="text-white text-lg text-center">
+              Whether you’re interested in speaking to our country management
+              team, enquiring about one of our products or partnering with us,
+              we want to hear from you.
+            </p>
+            <Button
+              text="Contact Us"
+              buttonStyle=" py-4 px-30 bg-white text-black text-md font-semibold rounded-sm"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="py-20 bg-[#2e2d2c] border-2 border-t-orange-800">
         <div className="max-w-[1580px] mx-auto">
           <div className="flex flex-col space-y-16">
             <div className="flex flex-row justify-between">
@@ -61,30 +84,30 @@ export default function Home() {
                   } `}
                 >
                   <div className="flex flex-col space-y-2">
-                    <p className="text-md font-thin text-white hover:text-orange-900 transition">
+                    <p className="text-md font-thin text-white hover:text-orange-800 transition">
                       Careers
                     </p>
-                    <p className="text-md font-thin text-white hover:text-orange-900 transition">
+                    <p className="text-md font-thin text-white hover:text-orange-800 transition">
                       Contact
                     </p>
-                    <p className="text-md font-thin text-white hover:text-orange-900 transition ">
+                    <p className="text-md font-thin text-white hover:text-orange-800 transition ">
                       About us
                     </p>
-                    <p className="text-md font-thin text-white hover:text-orange-900 transition">
+                    <p className="text-md font-thin text-white hover:text-orange-800 transition">
                       Locations
                     </p>
-                    <p className="text-md font-thin text-white hover:text-orange-900 transition">
+                    <p className="text-md font-thin text-white hover:text-orange-800 transition">
                       Privacy Policy
                     </p>
                   </div>
                   <div className="flex flex-col space-y-2">
-                    <p className="text-md font-thin text-white hover:text-orange-900 transition">
+                    <p className="text-md font-thin text-white hover:text-orange-800 transition">
                       Cookie Policy
                     </p>
-                    <p className="text-md font-thin text-white hover:text-orange-900 transition">
+                    <p className="text-md font-thin text-white hover:text-orange-800 transition">
                       Sitemap
                     </p>
-                    <p className="text-md font-thin text-white hover:text-orange-900 transition">
+                    <p className="text-md font-thin text-white hover:text-orange-800 transition">
                       Terms of Use
                     </p>
                   </div>
@@ -105,33 +128,33 @@ export default function Home() {
                   }`}
                 >
                   <div className="flex flex-col space-y-2">
-                    <p className="text-md font-thin text-white hover:text-orange-900 transition">
+                    <p className="text-md font-thin text-white hover:text-orange-800 transition">
                       Grains & Oilseeds
                     </p>
-                    <p className="text-md font-thin text-white hover:text-orange-900 transition">
+                    <p className="text-md font-thin text-white hover:text-orange-800 transition">
                       CFS & Oilseeds
                     </p>
-                    <p className="text-md font-thin text-white hover:text-orange-900 transition">
+                    <p className="text-md font-thin text-white hover:text-orange-800 transition">
                       Cotton
                     </p>
-                    <p className="text-md font-thin text-white hover:text-orange-900 transition">
+                    <p className="text-md font-thin text-white hover:text-orange-800 transition">
                       Editable Oils
                     </p>
-                    <p className="text-md font-thin text-white hover:text-orange-900 transition">
+                    <p className="text-md font-thin text-white hover:text-orange-800 transition">
                       Rice
                     </p>
                   </div>
                   <div className="flex flex-col space-y-2">
-                    <p className="text-md font-thin text-white hover:text-orange-900 transition">
+                    <p className="text-md font-thin text-white hover:text-orange-800 transition">
                       Rubber
                     </p>
-                    <p className="text-md font-thin text-white hover:text-orange-900 transition">
+                    <p className="text-md font-thin text-white hover:text-orange-800 transition">
                       Specialty Grains & Seed
                     </p>
-                    <p className="text-md font-thin text-white hover:text-orange-900 transition">
+                    <p className="text-md font-thin text-white hover:text-orange-800 transition">
                       Integrated Feed & Protein
                     </p>
-                    <p className="text-md font-thin text-white hover:text-orange-900 transition">
+                    <p className="text-md font-thin text-white hover:text-orange-800 transition">
                       Wood Products
                     </p>
                   </div>
@@ -146,7 +169,7 @@ export default function Home() {
                       alt="linkedin"
                       className="h-5 w-5 object-contain"
                     />
-                    <p className="text-md font-thin text-white hover:text-orange-900 transition">
+                    <p className="text-md font-thin text-white hover:text-orange-800 transition">
                       LinkedIn
                     </p>
                   </div>
@@ -156,7 +179,7 @@ export default function Home() {
                       alt="twitter"
                       className="object-contain h-5 w-5"
                     />
-                    <p className="text-md font-thin text-white hover:text-orange-900 transition">
+                    <p className="text-md font-thin text-white hover:text-orange-800 transition">
                       X
                     </p>
                   </div>
